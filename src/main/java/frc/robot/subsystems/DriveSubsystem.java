9e6d2d169c15fbec1dafcac17d52291a11802fac
AJAmelse <117790563+AJAmelse@users.noreply.github.com>
2024-06-06 20:14:31 -0500

    Revert back to phoenix 5 for SRX reasons

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,10 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -16,35 +14,43 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  TalonFX leftLeader;
-  TalonFX leftFollower;
-  TalonFX rightLeader;
-  TalonFX rightFollower;
-  DifferentialDrive drive;
-  public DigitalInput limitSwitch;
+   // Creating all our variables, we will initialize them and set their values later
+   WPI_TalonFX leftLeader;
+   WPI_TalonFX leftFollower;
+   WPI_TalonFX rightLeader;
+   WPI_TalonFX rightFollower;
+   DifferentialDrive drive;
+   public DigitalInput limitSwitch;
   
 
   public DriveSubsystem() {
-    leftLeader = new TalonFX(Constants.leftLeaderCANID);
-    leftFollower = new TalonFX(Constants.leftFollowerCANID);
-    rightLeader = new TalonFX(Constants.rightLeaderCANID);
-    rightFollower = new TalonFX(Constants.rightFollowerCANID);
-    leftLeader.getConfigurator().apply(new TalonFXConfiguration());
-    leftFollower.getConfigurator().apply(new TalonFXConfiguration());
-    rightLeader.getConfigurator().apply(new TalonFXConfiguration());
-    rightFollower.getConfigurator().apply(new TalonFXConfiguration());
-    leftLeader.setNeutralMode(NeutralModeValue.Brake);
-    rightLeader.setNeutralMode(NeutralModeValue.Brake);
-    leftFollower.setNeutralMode(NeutralModeValue.Brake);
-    rightFollower.setNeutralMode(NeutralModeValue.Brake);
+     //initialize motor controllers
+     leftLeader = new WPI_TalonFX(Constants.leftLeaderCANID);
+     leftFollower = new WPI_TalonFX(Constants.leftFollowerCANID);
+     rightLeader = new WPI_TalonFX(Constants.rightLeaderCANID);
+     rightFollower = new WPI_TalonFX(Constants.rightFollowerCANID);
+     leftLeader.configFactoryDefault();
+     leftFollower.configFactoryDefault();
+     rightLeader.configFactoryDefault();
+     rightFollower.configFactoryDefault();
+     //set motors to default to braking
+     leftLeader.setNeutralMode(NeutralMode.Brake);
+     rightLeader.setNeutralMode(NeutralMode.Brake);
+     leftFollower.setNeutralMode(NeutralMode.Brake);
+     rightFollower.setNeutralMode(NeutralMode.Brake);
 
     drive = new DifferentialDrive(
       leftLeader,
       rightLeader
     );
-    leftFollower.setControl(new Follower(leftLeader.getDeviceID(), false));
-    rightFollower.setControl(new Follower(leftLeader.getDeviceID(), false));
+    //Makes follower motors do the same thing as the leaders so that we don't have to pass arguments for all four
+    leftFollower.follow(leftLeader);
+    rightFollower.follow(rightLeader);
+
+    // inverts left motors from the right motors because they are inverted 180 degrees
+    leftFollower.setInverted(true);
+    leftLeader.setInverted(true);
+    
     
     
   }
